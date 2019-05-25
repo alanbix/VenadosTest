ef3d75473c1eae28dd60f50acb78dd92b7b38e81
Alan Barrera <alan.alexis64@gmail.com>
2019-05-25 00:29:48 -0500

    Modify navigation drawer to have the correct elements

--- app/src/main/java/com/alanbarrera/venadostest/MainActivity.java
+++ app/src/main/java/com/alanbarrera/venadostest/MainActivity.java
@@ -72,24 +72,21 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (id)
+        {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_statistics:
+                break;
+            case R.id.nav_players:
+                break;
+            default:
+                break;
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
