f5ec6bf2015c92e790122576f716b969064003e3
Alan Barrera <alan.alexis64@gmail.com>
2019-05-26 16:41:06 -0500

    Implemen game fragment in main activity

--- app/src/main/java/com/alanbarrera/venadostest/MainActivity.java
+++ app/src/main/java/com/alanbarrera/venadostest/MainActivity.java
@@ -1,22 +1,31 @@
 package com.alanbarrera.venadostest;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.view.View;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.util.Log;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.alanbarrera.venadostest.fragments.GamesFragment;
+import com.alanbarrera.venadostest.interfaces.IVenadosApiService;
+import com.alanbarrera.venadostest.models.Game;
+import com.alanbarrera.venadostest.network.VenadosApiService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, GamesFragment.OnListFragmentInteractionListener
+{
+    IVenadosApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,9 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        apiService = VenadosApiService.getService();
+        loadGames();
     }
 
     @Override
@@ -56,6 +68,7 @@ public class MainActivity extends AppCompatActivity
         switch (id)
         {
             case R.id.nav_home:
+                loadGames();
                 break;
             case R.id.nav_statistics:
                 break;
@@ -68,5 +81,38 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onListFragmentInteraction(Game item) {
+
+    }
+
+    private void loadGames()
+    {
+        Call<List<Game>> gamesCall = apiService.getGames();
+
+        gamesCall.enqueue(new Callback<List<Game>>()
+        {
+            @Override
+            public void onResponse(Call<List<Game>> call, Response<List<Game>> response)
+            {
+                if(response.isSuccessful() && response.body() != null)
+                {
+                    ArrayList<Game> games = new ArrayList<>(response.body());
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragmentContainer, GamesFragment.newInstance(games))
+                            .commit();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Game>> call, Throwable t)
+            {
+                Log.i("GamesCallback", "Failed to load elements");
+            }
+        });
     }
 }
