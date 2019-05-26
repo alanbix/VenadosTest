02d554ebcb2fea26659c62d6371bacf9c9c2def8
Alan Barrera <alan.alexis64@gmail.com>
2019-05-26 16:40:02 -0500

    Modify api service in order to implement callbacks in the actual activity.

--- app/src/main/java/com/alanbarrera/venadostest/network/VenadosApiService.java
+++ app/src/main/java/com/alanbarrera/venadostest/network/VenadosApiService.java
@@ -6,7 +6,6 @@ import com.alanbarrera.venadostest.interfaces.IVenadosApiService;
 import com.alanbarrera.venadostest.models.Game;
 import com.alanbarrera.venadostest.models.Player;
 import com.alanbarrera.venadostest.models.Statistic;
-import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -23,104 +22,32 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class VenadosApiService
 {
     private static final String BASE_URL = "https://venados.dacodes.mx/api/";
-    private Retrofit retrofit;
-    private IVenadosApiService venadosService;
+    private static IVenadosApiService venadosService = null;
 
-    private List<Game> games;
-    private List<Statistic> statistics;
-    private List<Player> players;
-
-    public VenadosApiService()
+    public static IVenadosApiService getService()
     {
-        retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(getGson()))
-                .client(getHttpClient())
-                .build();
-
-        venadosService = retrofit.create(IVenadosApiService.class);
-    }
-
-    public List<Game> getGames()
-    {
-        Call<List<Game>> gamesCall = venadosService.getGames();
-
-        gamesCall.enqueue(new Callback<List<Game>>()
+        if(venadosService == null)
         {
-            @Override
-            public void onResponse(Call<List<Game>> call, Response<List<Game>> response)
-            {
-                if(response.isSuccessful())
-                {
-                    games = response.body();
-                }
-            }
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(getGson()))
+                    .client(getHttpClient())
+                    .build();
 
-            @Override
-            public void onFailure(Call<List<Game>> call, Throwable t)
-            {
-                Log.i("GamesCallback", "Failed to load elements");
-            }
-        });
-        return games;
+            venadosService = retrofit.create(IVenadosApiService.class);
+        }
+
+        return venadosService;
     }
-
-    public List<Statistic> getStatistics()
-    {
-        Call<List<Statistic>> statisticsCall = venadosService.getStatistics();
-
-        statisticsCall.enqueue(new Callback<List<Statistic>>()
-        {
-            @Override
-            public void onResponse(Call<List<Statistic>> call, Response<List<Statistic>> response)
-            {
-                if(response.isSuccessful())
-                {
-                    statistics = response.body();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Statistic>> call, Throwable t)
-            {
-                Log.i("StatisticsCallback", "Failed to load elements");
-            }
-        });
-        return statistics;
-    }
-
-    public List<Player> getPlayers()
-    {
-        final Call<List<Player>> playersCall = venadosService.getPlayers();
-
-        playersCall.enqueue(new Callback<List<Player>>()
-        {
-            @Override
-            public void onResponse(Call<List<Player>> call, Response<List<Player>> response)
-            {
-                if(response.isSuccessful())
-                {
-                        players = response.body();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Player>> call, Throwable t)
-            {
-                Log.i("PlayersCallback", "Failed to load elements");
-            }
-        });
-        return players;
-    }
-
-    private Gson getGson()
+    
+    private static Gson getGson()
     {
         return new GsonBuilder()
                 .registerTypeAdapter(List.class, new DataDeserializer())
                 .create();
     }
 
-    private OkHttpClient getHttpClient()
+    private static OkHttpClient getHttpClient()
     {
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
         logging.setLevel(HttpLoggingInterceptor.Level.BODY);
